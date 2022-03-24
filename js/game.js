808de352f5c0230b3932cecb6d391b97f1cf3d9b
João Ferraz <jvkferraz@gmail.com>
2022-03-24 11:06:22 -0300

    Code formatting

--- js/game.js
+++ js/game.js
@@ -1,52 +1,52 @@
 class Game {
-    constructor (database){
-        this.database = database;
-        this.palavra = "";
-        this.numero = 0;
+  constructor(database) {
+    this.database = database;
+    this.palavra = "";
+    this.numero = 0;
+  }
+
+  init() {
+    this.numero = this.gerarNumero(this.database.length);
+    this.palavra = this.database[this.numero].toLowerCase().split("");
+  }
+
+  getWord() {
+    return this.palavra;
+  }
+
+  gerarNumero(interval) {
+    return Math.floor(Math.random() * interval);
+  }
+
+  isWon(input) {
+    return this.palavra.join("") === input.join("");
+  }
+
+  isLost(input) {
+    return tentativas === 6 && !game.isWon(input);
+  }
+
+  palavraExiste(arrayGuess) {
+    if (this.database.indexOf(arrayGuess.join("").toUpperCase()) != -1) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    init(){
-        this.numero = this.gerarNumero(this.database.length);
-        this.palavra = this.database[this.numero].toLowerCase().split("");
+  procurarPalavra(input) {
+    const result = [];
+    for (let i = 0; i < input.length; i++) {
+      let inputLetter = input[i].toLowerCase();
+      let wordLetter = this.palavra[i].toLowerCase();
+      if (inputLetter === wordLetter) {
+        result.push("rgba(115, 253, 115, 0.6)");
+      } else if (this.palavra.indexOf(inputLetter) != -1) {
+        result.push("rgba(253, 233, 115, 0.6)");
+      } else {
+        result.push("rgba(61, 61, 61, 0.2)");
+      }
     }
-
-    getWord(){
-        return this.palavra
-    }
-
-    gerarNumero(interval) {
-        return Math.floor(Math.random() * interval)
-    }
-
-    isWon (input){
-        return this.palavra.join("") === input.join("")
-    }
-
-    isLost (input){
-      return tentativas === 6 && !game.isWon(input)
-    }
-
-    palavraExiste (arrayGuess){
-      if (this.database.indexOf(arrayGuess.join("").toUpperCase()) != -1){
-     return true
- } else {return false}}
-
-    procurarPalavra (input) {
-        const result = [];
-        for (let i = 0; i < input.length; i++) {
-            let inputLetter = input[i].toLowerCase();
-            let wordLetter = this.palavra[i].toLowerCase();
-            if (inputLetter === wordLetter) {
-              result.push(("rgba(115, 253, 115, 0.6)"));
-            }
-            else if (this.palavra.indexOf(inputLetter) != -1) {
-              result.push(("rgba(253, 233, 115, 0.6)"));
-          }
-            else {
-              result.push(("rgba(61, 61, 61, 0.2)"));
-        }
-      } return result
-    }   
-
+    return result;
+  }
 }
-
--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ const game = new Game(arrayDePalavras);
 
 // DOM
 
-const div1 = document.getElementById('div1');
-const div2 = document.getElementById('div2');
-const div3 = document.getElementById('div3');
-const div4 = document.getElementById('div4');
-const div5 = document.getElementById('div5');
-const div6 = document.getElementById('div6');
-const btnTestar1 = document.getElementById('botaoTeste1');
-const btnTestar2 = document.getElementById('botaoTeste2');
-const btnTestar3 = document.getElementById('botaoTeste3');
-const btnTestar4 = document.getElementById('botaoTeste4');
-const btnTestar5 = document.getElementById('botaoTeste5');
-const btnTestar6 = document.getElementById('botaoTeste6');
+const div1 = document.getElementById("div1");
+const div2 = document.getElementById("div2");
+const div3 = document.getElementById("div3");
+const div4 = document.getElementById("div4");
+const div5 = document.getElementById("div5");
+const div6 = document.getElementById("div6");
+const btnTestar1 = document.getElementById("botaoTeste1");
+const btnTestar2 = document.getElementById("botaoTeste2");
+const btnTestar3 = document.getElementById("botaoTeste3");
+const btnTestar4 = document.getElementById("botaoTeste4");
+const btnTestar5 = document.getElementById("botaoTeste5");
+const btnTestar6 = document.getElementById("botaoTeste6");
 
 // Acompanhar o número de tentativas para checar o resultado
 
@@ -24,18 +24,24 @@ let tentativas = 0;
 // Funções do jogo
 
 function resultado(input) {
-    if (game.isWon(input)) {
-         alert("Parabéns, você ganhou!");
-         setTimeout(() => {restart()}, 1000)}
-    else if (game.isLost(input, tentativas)){
-        alert (`Fim de linha, a palavra era ${game.getWord().join("")}`);
-        setTimeout(() => {restart()}, 1000)}
-    else {tentativas ++}
+  if (game.isWon(input)) {
+    alert("Parabéns, você ganhou!");
+    setTimeout(() => {
+      restart();
+    }, 1000);
+  } else if (game.isLost(input, tentativas)) {
+    alert(`Fim de linha, a palavra era ${game.getWord().join("")}`);
+    setTimeout(() => {
+      restart();
+    }, 1000);
+  } else {
+    tentativas++;
+  }
 }
 
-function restart(){
-    location.reload();
-    return false;
+function restart() {
+  location.reload();
+  return false;
 }
 
 game.init();
@@ -44,129 +50,128 @@ console.log(`A palavra é: ${game.getWord().join("")}`);
 
 // Função de puxar o input, testar, mudar as cores, travar a row
 
-function puxar (id){
-    let span = document.getElementsByName(`${id}`); // Informo através da classe do botão o Id para puxar os inputs;
-    let arrayGuess = [];
-    arrayGuess = [];
+function puxar(id) {
+  let span = document.getElementsByName(`${id}`); // Informo através da classe do botão o Id para puxar os inputs;
+  let arrayGuess = [];
+  arrayGuess = [];
 
-    span.forEach((currentElement) => {arrayGuess.push(currentElement.value)}); // Passa para o array cada letra;
+  span.forEach((currentElement) => {
+    arrayGuess.push(currentElement.value);
+  }); // Passa para o array cada letra;
 
-    if (checar(arrayGuess) === true && game.palavraExiste(arrayGuess) === true){
-
+  if (checar(arrayGuess) === true && game.palavraExiste(arrayGuess) === true) {
     let cores = game.procurarPalavra(arrayGuess); // Compara no game.js a palavra e informa o retorno visual;
 
-    for (let i = 0; i < span.length; i++){ // Mudanças visuais
-        span[i].setAttribute("disabled", "disabled");
-        span[i].setAttribute("style", `background-color: ${cores[i]}`);
+    for (let i = 0; i < span.length; i++) {
+      // Mudanças visuais
+      span[i].setAttribute("disabled", "disabled");
+      span[i].setAttribute("style", `background-color: ${cores[i]}`);
     }
-    resultado (arrayGuess) // Checar vitória
-    return true} 
-
-    else {return false}
-
+    resultado(arrayGuess); // Checar vitória
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function checar(arrayGuess){
-        for (let i=0; i < arrayGuess.length; i++){
-            if(arrayGuess[i] === "")   
-               return false;
-        }
-        return true;
-     }
-
+function checar(arrayGuess) {
+  for (let i = 0; i < arrayGuess.length; i++) {
+    if (arrayGuess[i] === "") return false;
+  }
+  return true;
+}
 
 // Botões
 
-btnTestar1.addEventListener('click', (event) => {
-    if (puxar (event.target.classList[0]) === true){ // Função mestre
+btnTestar1.addEventListener("click", (event) => {
+  if (puxar(event.target.classList[0]) === true) {
+    // Função mestre
     btnTestar1.setAttribute("class", "no-show"); // Desligar botão e chamar a próxima fila
-    btnTestar2.classList.toggle ("no-show")
-    div2.classList.toggle ("no-show");
-    document.getElementById("input6").focus();}
-    else alert ("Erro")
-})
+    btnTestar2.classList.toggle("no-show");
+    div2.classList.toggle("no-show");
+    document.getElementById("input6").focus();
+  } else alert("Erro");
+});
 
-btnTestar2.addEventListener('click', (event) => {
-    if (puxar (event.target.classList[0]) === true){
+btnTestar2.addEventListener("click", (event) => {
+  if (puxar(event.target.classList[0]) === true) {
     btnTestar2.setAttribute("class", "no-show");
-    btnTestar3.classList.toggle ("no-show")
-    div3.classList.toggle ("no-show");
-    document.getElementById("input11").focus();}
-    else alert ("Erro")
-})
+    btnTestar3.classList.toggle("no-show");
+    div3.classList.toggle("no-show");
+    document.getElementById("input11").focus();
+  } else alert("Erro");
+});
 
-btnTestar3.addEventListener('click', (event) => {
-    if (puxar (event.target.classList[0]) === true){
-    puxar (event.target.classList[0]);
+btnTestar3.addEventListener("click", (event) => {
+  if (puxar(event.target.classList[0]) === true) {
+    puxar(event.target.classList[0]);
     btnTestar3.setAttribute("class", "no-show");
-    btnTestar4.classList.toggle ("no-show")
-    div4.classList.toggle ("no-show")
-    document.getElementById("input16").focus();}
-    else alert ("Erro")
-})
+    btnTestar4.classList.toggle("no-show");
+    div4.classList.toggle("no-show");
+    document.getElementById("input16").focus();
+  } else alert("Erro");
+});
 
-btnTestar4.addEventListener('click', (event) => {
-    if (puxar (event.target.classList[0]) === true){
+btnTestar4.addEventListener("click", (event) => {
+  if (puxar(event.target.classList[0]) === true) {
     btnTestar4.setAttribute("class", "no-show");
-    btnTestar5.classList.toggle ("no-show");
-    div5.classList.toggle ("no-show");
-    document.getElementById("input21").focus();}
-    else alert ("Erro")
-})
+    btnTestar5.classList.toggle("no-show");
+    div5.classList.toggle("no-show");
+    document.getElementById("input21").focus();
+  } else alert("Erro");
+});
 
-btnTestar5.addEventListener('click', (event) => {
-    if (puxar (event.target.classList[0]) === true){
+btnTestar5.addEventListener("click", (event) => {
+  if (puxar(event.target.classList[0]) === true) {
     btnTestar5.setAttribute("class", "no-show");
-    btnTestar6.classList.toggle ("no-show")
-    div6.classList.toggle ("no-show");
-    document.getElementById("input26").focus();}
-    else alert ("Erro")
-})
+    btnTestar6.classList.toggle("no-show");
+    div6.classList.toggle("no-show");
+    document.getElementById("input26").focus();
+  } else alert("Erro");
+});
 
-btnTestar6.addEventListener('click', (event) => {
-    if (puxar (event.target.classList[0]) === true){
-    btnTestar6.setAttribute("class", "no-show");}
-    else alert ("Erro")
-})
+btnTestar6.addEventListener("click", (event) => {
+  if (puxar(event.target.classList[0]) === true) {
+    btnTestar6.setAttribute("class", "no-show");
+  } else alert("Erro");
+});
 
 // Função - Só permitir letras + Focus
 
 function validate(arg) {
-    let element = document.getElementById(arg);
-    element.value = element.value.replace(/[^A-Za-z]+/, '');
-  }
+  let element = document.getElementById(arg);
+  element.value = element.value.replace(/[^A-Za-z]+/, "");
+}
 
 // Habilitar o enter
 
-document.addEventListener('keydown', (event) =>{
-    let botaovisivel = Array.from(document.getElementsByTagName('button')).filter(style =>
-        window.getComputedStyle(style).getPropertyValue('display') != 'none');
-        
-    if (event.key === 'Enter') {
-      botaovisivel[0].click();
-    }
-  });
+document.addEventListener("keydown", (event) => {
+  let botaovisivel = Array.from(document.getElementsByTagName("button")).filter(
+    (style) =>
+      window.getComputedStyle(style).getPropertyValue("display") != "none"
+  );
 
-  // Habilitar o Backspace
+  if (event.key === "Enter") {
+    botaovisivel[0].click();
+  }
+});
 
-document.addEventListener('keydown', (event) =>{
-    let element = document.activeElement
+// Habilitar o Backspace
 
+document.addEventListener("keydown", (event) => {
+  let element = document.activeElement;
 
-    if (event.key === 'Backspace') {
+  if (event.key === "Backspace") {
     event.preventDefault();
     element.value = "";
-      element.previousElementSibling.focus();
-    }
+    element.previousElementSibling.focus();
+  }
 
+  if (event.key === "ArrowLeft") {
+    element.previousElementSibling.focus();
+  }
 
-    if (event.key === 'ArrowLeft'){
-        element.previousElementSibling.focus()
-    }
-
-    if (event.key === 'ArrowRight'){
-        element.nextElementSibling.focus()
-    }
-  })
-
-
+  if (event.key === "ArrowRight") {
+    element.nextElementSibling.focus();
+  }
+});
